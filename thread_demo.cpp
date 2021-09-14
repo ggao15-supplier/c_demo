be3d9ac451ad926a690577d416a25807de51fd1d
gggao <gggao@telenav.cn>
2021-09-14 10:24:08 +0800

    modify thread

--- thread_demo.cpp
+++ thread_demo.cpp
@@ -6,18 +6,52 @@
 #include <thread>
 #define NUM 6
 using namespace std;
+using namespace this_thread;
+using namespace chrono;
+class StandThreadHandler {
+ private:
+  thread* myThread;
+  int index;
 
-void handler(void* arg) {
-  int second = *((int*)arg);
-  std::this_thread::sleep_for(std::chrono::seconds(second));
-  cout << "thread11:" << this_thread::get_id << ";" << second << endl;
+ public:
+  StandThreadHandler(thread mt, int n) : myThread(mt), index(n) {
+    cout << "crate" << endl;
+  }
+  thread* getMyThread() { return myThread; }
+  int getIndex() { return index; }
+};
+void standHanlder(void* arg) {
+  StandThreadHandler* item = (StandThreadHandler*)arg;
+  sleep_for(seconds(1));
+  cout << "standThread" << this_thread::get_id << ";" << item->getIndex()
+       << endl;
+  item->getMyThread()->detach();
 }
-int main() {
+
+void* cHandler(void* arg) {
+  int index = *((int*)arg);
+  sleep(1);
+  cout << "thread:" << index << endl;
+  pthread_detach(pthread_self());
+  return 0;
+}
+// some problem , not work
+void cThread() {
+  void* status;
   pthread_t tids[NUM];
   for (int i = 0; i < NUM; i++) {
-    thread mythread = thread(handler, &i);
-    mythread.join();
+    pthread_create(&tids[i], NULL, cHandler, &i);
+    pthread_join(tids[i], &status);
   }
-
+}
+void standThread() {
+  for (int i = 0; i < NUM; i++) {
+    thread myThread = thread(standHanlder, (&StandThreadHandler(&myThread, i)));
+    myThread.join();
+  }
+}
+int main() {
+  cThread();
+  standThread();
   return 0;
 }
