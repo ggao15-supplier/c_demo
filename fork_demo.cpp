45a7b843ffac174930269564648bd68e1c27f0d6
ggao15-supplier <g1195643514@163.com>
2021-09-22 17:41:00 +0800

    add auto suggestion

--- fork_demo.cpp
+++ fork_demo.cpp
@@ -4,16 +4,19 @@
 #include <string>
 
 using namespace std;
+// https://blog.csdn.net/yx903520/article/details/106359598
 
+// 自动补全功能的配置
 void onlyFork() {
   cout << "fork process.." << endl;
   pid_t cpid = fork();
   if (cpid < 0) {
     cout << "fork failed" << endl;
   } else if (cpid > 0) {
-    cout << "fork from parent proccess,the pid is:" << cpid << endl;
+    cout << "fork from parent proccess,the cpid is:" << cpid
+         << " ;the getPid is: " << getpid() << endl;
   } else {
-    cout << "is child proccess,the pid is:" << cpid << endl;
+    cout << "is child proccess,the pid is:" << getpid() << endl;
   }
 }
 
